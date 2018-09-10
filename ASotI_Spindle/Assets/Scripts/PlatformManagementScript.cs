922cc1d9e6d62487d11918a946a4471e3ab21637
ceheckel <checkel13@gmail.com>
2018-09-10 14:56:32 -0400

    Initial Dynamic Platform Creation, Deletion, and Translation

--- ASotI_Spindle/Assets/Scripts/PlatformManagementScript.cs
+++ ASotI_Spindle/Assets/Scripts/PlatformManagementScript.cs
@@ -5,17 +5,17 @@ using UnityEngine;
 public class PlatformManagementScript : MonoBehaviour {
 
 	// references
-	public float speedScale;
 	public GameObject ball;
 	public GameObject[] platTypes;
 
-	private GameObject[] platforms;
+	internal GameObject[] platforms;
 	private int index; // index counter for next open spot in platforms array
-	private readonly int numPlats = 10;
+	private readonly int numPlats = 4;
 	private static PlatformManagementScript instance;
 
 	private void Awake()
 	{
+		// create singleton
 		if (instance != null && instance != this) { Destroy(this.gameObject); }
 		else { instance = this;	}
 	}
@@ -25,48 +25,83 @@ public class PlatformManagementScript : MonoBehaviour {
 		platforms = new GameObject[numPlats];
 		index = 0;
 
-		FindPlatforms();
+		Debug.Log("index0: " + index);
+
+		int numFound = FindPlatforms();
+
+		Debug.Log("index1: " + index);
+		Debug.Log("found1: " + numFound);
+
+		if (numFound < numPlats)
+		{
+			for (int i = numFound; i < numPlats; i += 1)
+			{
+				AddPlatform();
+			}
+		}
 	}
 
-	private void FindPlatforms()
+	private int FindPlatforms()
 	{
-		platforms = GameObject.FindGameObjectsWithTag("Platform");
-		if (platforms == null || platforms.Length == 0)
+		// find all existing platforms in the scene
+		GameObject[] temp = GameObject.FindGameObjectsWithTag("Platform");
+
+		// if none were found ...
+		if (temp == null || temp.Length == 0)
 		{
+			// ... notify the boss
 			Debug.LogWarning("FindPlatforms: No Platforms were found");
-			return;
+			return 0;
 		}
 
-		index = platforms.Length;
+		// Since FindGameObjectsWithTag() returns a new array of the perfect size
+		// we need to copy the search results into our management array.
+		for (int i = 0; i < temp.Length; i += 1)
+		{
+			platforms[i] = temp[i];
+		}
+
+		// mark the next available index
+		index = temp.Length;
+		return index;
 	}
 
-	private void AddPlatform()
+	internal void AddPlatform()
 	{
-		// create new object at the bottom of the post
-		platforms[index] = Instantiate
-			(
-				RandomPlatform(),
-				new Vector3
-				(
-					platforms[PrevEmpty()].transform.position.x,
-					platforms[PrevEmpty()].transform.position.y - 5,
-					platforms[PrevEmpty()].transform.position.z
-				),
-				platforms[PrevEmpty()].transform.rotation,
-				platforms[PrevEmpty()].transform.parent
-			);
-		
-		// manipulate new platform
+		Vector3 pos;
+		Quaternion rot;
+		Transform par = GameObject.Find("Platforms").transform;
 
+		// get platform template
+		if (platforms[PrevIndex()] == null)
+		{
+
+			Debug.Log("index2: " + index);
+
+			pos = new Vector3(0, -5, 0);
+			rot = new Quaternion();
+		}
+		else
+		{
+			pos = new Vector3(0, platforms[PrevIndex()].transform.position.y - 5, 0);
+			rot = platforms[PrevIndex()].transform.rotation;
+
+			Debug.Log("index3: " + index);
+
+		}
+
+		// create new object at the bottom of the post
+		platforms[index] = Instantiate(RandomPlatform(), pos, rot, par);
 
 		// manage counter
 		IncIndex();
+
+		Debug.Log("index4: " + index);
+
 	}
 
-	private void RemovePlatform()
+	internal void RemovePlatform()
 	{
-		DecIndex();
-
 		Destroy(platforms[index]);
 		platforms[index] = null;
 	}
@@ -76,8 +111,8 @@ public class PlatformManagementScript : MonoBehaviour {
 		return platTypes[Random.Range(0, platTypes.Length)];
 	}
 
-	private int PrevEmpty() { return (index == 0 ? numPlats - 1 : index - 1); }
-	private int NextEmpty() { return (index + 1) % numPlats; }
+	private int PrevIndex() { return (index == 0 ? numPlats - 1 : index - 1); }
+	private int NextIndex() { return (index + 1) % numPlats; }
 	private void IncIndex() { index = (index + 1) % numPlats; }
 	private void DecIndex() { index = (index == 0 ? numPlats - 1 : index - 1); }
 }
---
+++ ASotI_Spindle/Assets/Scripts/RaisePlatformScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaisePlatformScript : MonoBehaviour
+{
+
+	public float speedScale;
+
+	private GameObject gc;
+	private Vector3 target;
+	private bool needsRaise;
+
+	private void Awake()
+	{
+		gc = GameObject.Find("GameController");
+		if (gc == null) { Debug.LogWarning("RaisePlatformScript: gc not found"); }
+
+		target = new Vector3(0, 5, 0);
+		needsRaise = false;
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		// Begin platform raising process
+		needsRaise = true;
+	}
+
+	private void Update()
+	{
+		if (needsRaise)
+		{
+			foreach (GameObject p in gc.GetComponent<PlatformManagementScript>().platforms)
+			{
+				if (p != null)
+				{
+					// move platform up
+					p.transform.position = Vector3.MoveTowards
+						(
+							p.transform.position,
+							target,
+							Time.deltaTime * speedScale
+						);
+
+					// when a platform reaches the top ...
+					if (p.transform.position.y >= 5)
+					{
+						// ... Stop raising platforms
+						needsRaise = false;
+
+						// ... Destroy platform, p
+						Debug.Log("Dead");
+						gc.GetComponent<PlatformManagementScript>().RemovePlatform();
+
+						// ... Create new platform
+						Debug.Log("Create");
+						gc.GetComponent<PlatformManagementScript>().AddPlatform();
+					}
+				}
+			}
+		}
+	}
+}
